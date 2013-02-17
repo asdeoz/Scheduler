4f5238af6dbdbaf9e0e0cfe3565da9fc10d29f59
Carles Garcia <asdeoz@gmail.com>
2013-02-17 22:56:24 +0100

    Masked input, blockday and localdb
    Returned to localdb instead of localhost
    BlockDay added to finally replace the WeekDay
    Create.cshtml modifies to start accepting the blockdays
    New javascript file to use maskedinput

--- SchedulerModels/Entities/Block.cs
+++ SchedulerModels/Entities/Block.cs
@@ -11,29 +11,29 @@ namespace SchedulerModels.Entities
     public class Block
     {
         public int BlockId { get; set; }
-        [Required(ErrorMessage="The block needs a name to identify it")]
+        [Required(ErrorMessage = "The block needs a name to identify it")]
         [MaxLength(50)]
         public string Name { get; set; }
         [MaxLength(250)]
         public string Description { get; set; }
-        [Display(Name="Is it active?")]
+        [Display(Name = "Is it active?")]
         public bool IsActive { get; set; }
-        [Display(Name="Start date")]
+        [Display(Name = "Start date")]
         [DataType(DataType.Date)]
         public DateTime? StartDate { get; set; }
-        [Display(Name="End date")]
+        [Display(Name = "End date")]
         [DataType(DataType.Date)]
         public DateTime? EndDate { get; set; }
         public int? GradeLevelId { get; set; }
         public int? TeacherId { get; set; }
 
         [ForeignKey("GradeLevelId")]
-        //[Required(ErrorMessage="A block must be in a grade level")]
         public virtual GradeLevel Grade { get; set; }
         public virtual List<Student> Students { get; set; }
-        public virtual List<DayOfWeek> ScheduledDays { get; set; }
+        public virtual List<BlockDay> ScheduledDays { get; set; }
         [ForeignKey("TeacherId")]
-        //[Required(ErrorMessage="A block must have a teacher")]
         public virtual Teacher Teacher { get; set; }
+
+        //public virtual List<DayOfWeek> ScheduledDays { get; set; }
     }
 }
---
+++ SchedulerModels/Entities/BlockDay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchedulerModels.Entities
+{
+    public class BlockDay
+    {
+        public int BlockDayId { get; set; }
+        public int Day { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public int? BlockId { get; set; }
+        [ForeignKey("BlockId")]
+        public virtual Block Block { get; set; }
+    }
+}
